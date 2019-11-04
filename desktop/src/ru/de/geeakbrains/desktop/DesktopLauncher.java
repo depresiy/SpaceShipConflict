16c23d07fa5feb203711abf0a66122fd7036d107
depresiy <depresiy@mail.ru>
2019-11-05 01:56:47 +0500

    HW

---
+++ desktop/src/ru/de/geeakbrains/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.de.geeakbrains.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.de.geeakbrains.SpaceShipConflict;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShipConflict(), config);
+	}
+}
